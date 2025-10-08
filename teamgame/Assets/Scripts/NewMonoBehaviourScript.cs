05856c45658f1c6d348ce3fa8504d50c828a4203
171650 <171650@student.talland.nl>
2025-10-08 11:11:52 +0200

    test conficten

--- teamgame/Assets/Scripts/NewMonoBehaviourScript.cs
+++ teamgame/Assets/Scripts/NewMonoBehaviourScript.cs
@@ -5,7 +5,7 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // dit is regel 8
     }
 
     // Update is called once per frame
