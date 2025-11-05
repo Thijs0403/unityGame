394d576002cbc4ddbe493653d21b0c230837e105
Thijs0403 <174294@student.horizoncollege.nl>
2025-11-05 09:23:59 +0100

    inverntory system start

--- teamgame/Assets/Scripts/InventoryManager.cs
+++ teamgame/Assets/Scripts/InventoryManager.cs
@@ -1,16 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class NewMonoBehaviourScript1 : MonoBehaviour
+public class InventoryManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<List<string>> inventoryArray = new List<List<string>>();
+
+    public void AddItem(string itemName, string description)
     {
-        
+        List<string> newItem = new List<string>();
+        newItem.Add(itemName + "_" + (inventoryArray.Count + 1));
+        newItem.Add(description);
+        newItem.Add("ID_" + (inventoryArray.Count + 1));
+        inventoryArray.Add(newItem);
+
+        Debug.Log("Added a new item!");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DebugPrint()
     {
-        
+        for (int i = 0; i < inventoryArray.Count; i++)
+        {
+            List<string> item = inventoryArray[i];
+            Debug.Log("Item " + i + ": " + item[0] + ", " + item[1] + ", " + item[2]);
+        }
     }
 }
--- teamgame/Assets/Scripts/NewMonoBehaviourScript.cs
+++ teamgame/Assets/Scripts/NewMonoBehaviourScript.cs
@@ -2,17 +2,35 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    private InventoryManager inventoryManager;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // dit is regel 8
+        inventoryManager = GetComponent<InventoryManager>();
     }
 
-//Hallo Sjaak
+    //Hallo Sjaak
 
     // Update is called once per frame
     void Update()
     {
-        //Test Thijs 
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {   
+            inventoryManager.DebugPrint();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            inventoryManager.AddItem("appel", "lekker fruit");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            inventoryManager.AddItem("swaard", "wapen om mee te doden");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            inventoryManager.AddItem("schild", "een stuk hout");
+        }
     }
 }
