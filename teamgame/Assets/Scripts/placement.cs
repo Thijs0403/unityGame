c6a1f35fc9a33fb5d1bd4608880b7977bbe804c1
171650 <171650@student.talland.nl>
2025-12-17 11:45:37 +0100

    muren plaatsen en slopen

--- teamgame/Assets/Scripts/placement.cs
+++ teamgame/Assets/Scripts/placement.cs
@@ -3,18 +3,32 @@ using UnityEngine;
 public class placement : MonoBehaviour
 {
     public GameObject wall;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float placedistance = 5f;
+    public GameObject player;
+    public Camera cam;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if(Input.GetButtonDown("Fire2"))
         {
-            Instantiate(Place);
+            Place();
+        }
+    }
+    void Place()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, placedistance))
+        {
+            float playerRot = player.transform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0, playerRot, 0);
+            Instantiate(wall, hit.point, rotation);
         }
     }
 }
